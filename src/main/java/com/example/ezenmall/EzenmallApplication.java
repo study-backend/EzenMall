767ee991f03e3c81a6b720cb17906989d639c449
jisun <jisunnala@gmail.com>
2022-06-12 18:19:33 +0900

    initial commit

---
+++ src/main/java/com/example/ezenmall/EzenmallApplication.java
@@ -0,0 +1,16 @@
+package com.example.ezenmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzenmallApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(EzenmallApplication.class, args);
+	}
+
+
+
+}
